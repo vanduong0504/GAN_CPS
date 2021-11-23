a7d055654ec320e62652601eac3465d7e23b4fd7
Trong Van <60954708+vanduong0504@users.noreply.github.com>
2021-11-23 18:54:12 +0700

    Update discriminators.py

--- models/modules/discriminators.py
+++ models/modules/discriminators.py
@@ -10,12 +10,18 @@ from models.networks import BaseNetwork
 
 
 
-class NLayerDiscriminator(BaseNetwork):
+import argparse
+import functools
+
+import numpy as np
+from torch import nn
+from torch.nn import functional as F
+
+class NLayerDiscriminator(nn.Module):
     """Defines a PatchGAN discriminator"""
 
-    def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d):
+    def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.InstanceNorm2d):
         """Construct a PatchGAN discriminator
-
         Parameters:
             input_nc (int)  -- the number of channels in input images
             ndf (int)       -- the number of filters in the last conv layer
@@ -28,34 +34,40 @@ class NLayerDiscriminator(BaseNetwork):
         else:
             use_bias = norm_layer == nn.InstanceNorm2d
 
-        kw = 4
-        padw = 1
-        sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]
+        ker = 4
+		padw = 1
+		s = 2 
+
+        sequence = [nn.Conv2d(input_nc, ndf, kernel_size=ker, stride=s, padding=padw), nn.LeakyReLU(0.2, True)] #1
         nf_mult = 1
+
+
         for n in range(1, n_layers):  # gradually increase the number of filters
             nf_mult_prev = nf_mult
             nf_mult = min(2 ** n, 8)
             sequence += [
-                nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=2, padding=padw, bias=use_bias),
+                nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=ker, stride=s, padding=padw, bias=use_bias), #2 , #3
                 norm_layer(ndf * nf_mult),
                 nn.LeakyReLU(0.2, True)
             ]
+			ker = 3
+			s = 1
 
         nf_mult_prev = nf_mult
         nf_mult = min(2 ** n_layers, 8)
         sequence += [
-            nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias),
+            nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=4, stride=2, padding=padw, bias=use_bias), #4
             norm_layer(ndf * nf_mult),
             nn.LeakyReLU(0.2, True)
         ]
 
-        sequence += [
-            nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]  # output 1 channel prediction map
+        sequence += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=3, stride=1, padding=padw)]  # output 1 channel prediction map
         self.model = nn.Sequential(*sequence)
 
     def forward(self, input):
         """Standard forward."""
         return self.model(input)
+
 
 
 class PixelDiscriminator(BaseNetwork):
